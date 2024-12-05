b75ff3809243929e8be469ee218a16bb9c9f07a0
Nagy Ahmed <nagya4546@gmail.com>
2024-12-05 02:27:21 +0200

    Update Main.java Adding Nagy name
    my name

--- Pet_Adoption/src/Main.java
+++ Pet_Adoption/src/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException  
     {
+        System.out.print("Welcome this is Nagy Ahmed ");
         Application app=new Application();
         app.start();
         
